624a7982dc27f4eb6617e2aa5ff6d468774e5359
charlieNg014 <cnguyen.swin@gmail.com>
2020-02-08 20:19:10 +1100

    Add review page

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -3,6 +3,7 @@ import Info from "../Info"
 import Footer from "../Footer"
 import {Link} from "react-router-dom"
 import {useSelector} from 'react-redux'
+import reviewimage from "../../images/review-image.jpg"
 
 export default function ProductDetails() {
     const productDetails = useSelector(state => state.getProductDetails.productDetails);
@@ -21,7 +22,7 @@ export default function ProductDetails() {
             </div>
             <div className="product-display row">
                 <div className="col-md-5">
-                    <img className="details-image" src={productDetails.image} alt ="details image" /> 
+                    <img className="details-image" src={productDetails.image} alt ="detailsimage" /> 
                 </div>
                 <div className=" col-md-7 details">
                 <h1 className="details-title">{productDetails.title}</h1>
@@ -36,6 +37,52 @@ export default function ProductDetails() {
                     <p className="details-category"><strong> CATEGORIES:</strong>{productDetails.type}</p>
                 </div>
                 <hr className="details-"/>
+                </div>
+            </div>
+            <div className="product-additionalInfo">
+                <div className="addInfo-title row">
+                    <Link className="col-md-5 title-first" onClick={console.log("test")}>Description</Link>
+                    <Link className="col-md-2 title-second">Reviews</Link>
+                    <Link className="col-md-5 title-third">Addtional Info</Link>
+                </div>
+                <div className="additionalInfo-details-desc">
+                    <h2 className="addInfo-title">Ingredients:</h2>
+                    <p className="addInfo-details">infor about ingredients goes here</p>
+                    <h2 className="addInfo-title" style={{marginTop: 30}}>Preparation:</h2>
+                    <p className="addInfo-details">infor about preparation goes here</p>
+                </div>
+
+                <div className="additionalInfo-details-review">
+                    <div className="review row">
+                        <div className="col-md-1">
+                            <img className="review-image" src={reviewimage} alt="reviewimage" />
+                        </div>
+                        <div className="review-info col-md-11">
+                            <h2 className="addInfo-title" style={{display:"flex"}}>By name: </h2>
+                            <h2 className="review-date">Date</h2> 
+                            <p className="addInfo-details">interview goes here</p>
+                        </div>
+                    </div>
+                    <div className="review-instant">
+                        <h2 className="instant-title">Add your review</h2>
+                        <div className="instant-name row">
+                            <div className="form-group col-md-6">
+                                <input className="form-control special-left" type="text" placeholder="Your Name" />
+                            </div>
+                            <div className="form-group col-md-6">
+                                <input className="form-control special-right" type="text" placeholder="Your Email" />
+                            </div>
+                        </div>
+                        <div className="instant-review row">
+                            <textarea className="col-md-12 form-control" rows="6" placeholder="Write your review here"></textarea>
+                        </div>
+                        <div class="form-group submit">
+                            <button class="detail-btn">Submit Review</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="additionalInfo-details-add">
+                    <p className="addInfo-details">Additional information goes here</p>
                 </div>
             </div>
                    
--- src/components/products/ProductDisplay.js
+++ src/components/products/ProductDisplay.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {bakeStore} from '../../data'
-import {useSelector, useDispatch} from 'react-redux'
+import {useSelector} from 'react-redux'
 import ProductList from './ProductList';
 
 export default function ProductDisplay() {
